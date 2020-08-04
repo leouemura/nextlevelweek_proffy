a5d9dce38e0984a5736ce97568ecc0d924e18802
leouemura <leo.uemura.s3@gmail.com>
2020-08-03 23:32:00 -0300

    Link to prevent reload in LandingPage buttons

--- frontweb/src/pages/Landing/index.tsx
+++ frontweb/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './styles.css';
 
 import logoImg from '../../assets/images/logo.svg'
@@ -19,15 +20,15 @@ function Landing(){
                 </div>
                 <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
                 <div className="buttons-container">
-                    <a href="/" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="/" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt=""/>
                         Dar Aula
-                    </a>
+                    </Link>
                 </div>
                 
                 <span className="total-connections">
